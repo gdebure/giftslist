3b2c73dd293b676fe80fac89228db5a974a49cdc
Guillaume de Bure <guillaume.debure@gmail.com>
2012-11-21 07:48:06 +0100

    Use AwesomeFont for icons, and some formatting

--- giftslist/settings.py
+++ giftslist/settings.py
@@ -70,7 +70,8 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
-    '/home/guillaume/Developpement/git/CREAM/static/bootstrap',
+    '/home/guillaume/Developpement/bootstrap/',
+    '/home/guillaume/Developpement/FontAwesome/',
 )
 
 # List of finder classes that know how to find static files in
